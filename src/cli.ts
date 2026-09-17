1372eeb8cba6699c7d3ef7a861735a27563ef244
Teemu H <44917122+swd1tn002@users.noreply.github.com>
2026-09-17 06:56:15 +0000

    Added help and checking of correct argument count

--- src/cli.ts
+++ src/cli.ts
@@ -1,12 +1,26 @@
 
 export function readParams(...paramNames: string[]): string[] {
+    const help = `Usage: node ${process.argv[1].split("/").pop()} ${paramNames.map(n => `<${n}>`).join(" ")}`;
+
     // omit the first two arguments (node and script path)
-    const argv = process.argv.slice(2);
+    const args = process.argv.slice(2);
+
+    if (args.includes("--help") || args.includes("-h")) {
+        console.log(help);
+        process.exit(0);
+    }
+
+    if (args.length !== paramNames.length) {
+        console.error(`Invalid number of arguments. Expected ${paramNames.length}, got ${args.length}.`);
+        console.error(help);
+        process.exit(1);
+    }
 
     return paramNames.map((name, index) => {
-        const userInput = argv[index];
+        const userInput = args[index];
         if (!userInput) {
             console.error(`Missing parameter: ${name}`);
+            console.error(help);
             process.exit(1);
         }
         return userInput;
